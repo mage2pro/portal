8e36356b6b8c8a38c909ddb47a9be4013d2da6c7
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-05-05 20:57:43 +0300

    no message

---
+++ T/Basic.php
@@ -0,0 +1,7 @@
+<?php
+namespace Dfe\Cms\T;
+// 2017-05-05
+final class Basic extends TestCase {
+	/** @test 2017-05-05 */
+	function t01() {}
+}
---
+++ T/TestCase.php
@@ -0,0 +1,7 @@
+<?php
+namespace Dfe\Cms\T;
+/**
+ * 2017-05-05
+ * @see \Dfe\Cms\T\Basic
+ */
+abstract class TestCase extends \Df\Core\TestCase {}
