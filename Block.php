4181d186e63b9f3ecbf7fc06385c7954d2437d77
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-05-06 12:07:47 +0300

    0.0.8

--- Block.php
+++ Block.php
@@ -1,15 +1,15 @@
 <?php
 namespace Dfe\Cms;
-use Magento\Framework\View\Element\AbstractBlock as _P;
+use Magento\Framework\View\Element\Template as _P;
 // 2017-05-05
 /** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
 class Block extends _P {
 	/**
 	 * 2017-05-05
 	 * @override
-	 * @see _P::_toHtml()
-	 * @used-by \Magento\Framework\View\Element\AbstractBlock::toHtml()
+	 * @see \Magento\Framework\View\Element\Template::getTemplate()
+	 * @see \Magento\Payment\Block\Info::$_template
 	 * @return string
 	 */
-	final protected function _toHtml() {return 'HELLO, WORLD!';}
+	final function getTemplate() {return df_cfg('df_cms/general/content') . '::main.phtml';}
 }
