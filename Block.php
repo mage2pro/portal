09a10c6958bf6633fb0f6429652eac9edc1d2b85
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-05-07 23:36:58 +0300

    0.2.4

--- Block.php
+++ Block.php
@@ -6,10 +6,23 @@ use Magento\Framework\View\Element\Template as _P;
 class Block extends _P {
 	/**
 	 * 2017-05-05
+	 * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
 	 * @override
 	 * @see \Magento\Framework\View\Element\Template::getTemplate()
 	 * @see \Magento\Payment\Block\Info::$_template
 	 * @return string
 	 */
-	final function getTemplate() {return dfe_cms_module_name() . '::main.phtml';}
+	function getTemplate() {return dfe_cms_module_name() . '::main.phtml';}
+
+	/**
+	 * 2017-05-07
+	 * @override
+	 * @see \Magento\Framework\View\Element\AbstractBlock::_prepareLayout()
+	 * @used-by \Magento\Framework\View\Element\AbstractBlock::setLayout()
+	 */
+	protected function _prepareLayout() {
+		if (dfe_cms_cfg_g()->enable()) {
+			df_page_config()->addPageAsset('Dfe_Cms::main.css');
+		}
+	}
 }
