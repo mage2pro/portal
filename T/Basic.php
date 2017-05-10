55844ee6fbb970fba3289474fc2fea475780b936
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-05-10 04:55:33 +0300

    no message

--- T/Basic.php
+++ T/Basic.php
@@ -2,6 +2,9 @@
 namespace Dfe\Cms\T;
 // 2017-05-05
 final class Basic extends TestCase {
-	/** @test 2017-05-05 */
+	/** 2017-05-05 */
 	function t01() {echo df_dump(dfe_cms_plugins());}
+
+	/** @test 2017-05-10 */
+	function t02() {echo df_dump(df_github_key());}
 }
