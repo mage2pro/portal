1e4a63696f7a262742abc5a88881b273633f58d8
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-07 00:59:33 +0000

    https://github.com/mage2pro/core/issues/168

--- Test/Basic.php
+++ Test/Basic.php
@@ -3,8 +3,8 @@ namespace Dfe\Portal\Test;
 # 2017-05-05
 final class Basic extends TestCase {
 	/** 2017-05-05 */
-	function t01() {print_r(df_dump(dfe_portal_plugins()));}
+	function t01():void {print_r(df_dump(dfe_portal_plugins()));}
 
 	/** 2017-05-10 @test */
-	function t02() {print_r(df_dump(df_github_repo_version('mage2pro/stripe')));}
+	function t02():void {print_r(df_dump(df_github_repo_version('mage2pro/stripe')));}
 }
